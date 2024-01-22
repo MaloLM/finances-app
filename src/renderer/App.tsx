acd32ff8994a4fcca1849b29dab44316d25b04a8
wawkadura <elwalid.kadura@gmail.com>
2024-01-22 16:54:53 +0100

    feat(renderer/App): refactoring data retrieval

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,127 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useIpcRenderer } from "./api/electron";
 import "./styles/App.css";
 import { TargetAllocationMaintenance } from "./pages/TargetAllocationMaintenance";
+import { TamFormData, parseTamFormData } from "./utils";
 
 const App = () => {
-    return (
-        <div className="App">
-            <div className="header">
-                <div className="menu-icon" onClick={() => toggleMenu()}>
-                    <div className="menu-line"></div>
-                    <div className="menu-line"></div>
-                    <div className="menu-line"></div>
-                </div>
-                <span id="feature-title"></span>
-            </div>
-            <div className="menu-dropdown" id="menuDropdown">
-                <a
-                    href="#"
-                    // onClick={() => loadFeature('target_allocation_maintenance/index.html','Target Allocation Maintenance')}
-                >Target Allocation Maintenance</a>
-                <a
-                    href="#"
-                    // onClick={() => loadFeature('feature_2/index.html','Another feature')}
-                >Another feature</a>
-            </div>
-            <TargetAllocationMaintenance />
-        </div>
-    );
-};
+  const [data, setData] = React.useState<TamFormData>({} as TamFormData);
+  const [dataRetreived, setDataRetreived] = React.useState<boolean>(false);
+  const { sendRequestData, onResponseData } = useIpcRenderer();
 
-const { sendRequestData, sendWriteData, onResponseData, onWriteResponse } = useIpcRenderer();
+  useEffect(() => {
+    // Define the response handler
+    const handleResponse = (event, responseData) => {
+      if (event.error) {
+        console.error(event.error);
+      } else {
+        setData(parseTamFormData(responseData));
+      }
+      setDataRetreived(true);
+    };
+    // Set up the listener for the response
+    onResponseData(handleResponse);
+    sendRequestData();
+  }, []);
 
-function toggleMenu(): void {
-  var menu = document.getElementById("menuDropdown") as HTMLDivElement;
-  if (menu.style.display === "none" || menu.style.display === "") {
-    menu.style.display = "flex";
-  } else {
-    menu.style.display = "none";
+  function toggleMenu(): void {
+    var menu = document.getElementById("menuDropdown") as HTMLDivElement;
+    if (menu.style.display === "none" || menu.style.display === "") {
+      menu.style.display = "flex";
+    } else {
+      menu.style.display = "none";
+    }
   }
-}
 
-// Define a type for the message event data
-// type CustomMessageEventData = {
-//   action: string;
-//   data?: any;
-// };
-
-// Convert 'require' to ES6 import
-// window.addEventListener("message", (event: MessageEvent) => {
-//   const iframe = document.getElementById("feature-iframe") as HTMLIFrameElement;
-//   const eventData = event.data as CustomMessageEventData;
-
-//   if (
-//     event.source === iframe.contentWindow &&
-//     eventData.action === "requestData"
-//   ) {
-//     sendRequestData();
-//   }
-// });
-
-// window.addEventListener("message", (event: MessageEvent) => {
-//   const iframe = document.getElementById("feature-iframe") as HTMLIFrameElement;
-//   const eventData = event.data as CustomMessageEventData;
-
-//   if (
-//     event.source === iframe.contentWindow &&
-//     eventData.action === "sendData"
-//   ) {
-//     sendWriteData(eventData.data);
-//   }
-// });
-
-// onResponseData( (_event: any, data: any) => {
-//   const iframe = document.getElementById("feature-iframe") as HTMLIFrameElement;
-//   iframe.contentWindow?.postMessage({ action: "responseData", data: data }, "*");
-// });
-
-// onWriteResponse( (_event: any, response: any) => {
-//   const iframe = document.getElementById("feature-iframe") as HTMLIFrameElement;
-//   iframe.contentWindow?.postMessage(
-//     { action: "writeResponse", data: response },
-//     "*"
-//   );
-// });
-
-
-
-// function loadFeature(featurePath: string, featureName: string): void {
-//   const iframe = document.getElementById("feature-iframe") as HTMLIFrameElement;
-//   const featureTitle = document.getElementById("feature-title") as HTMLDivElement;
-//   const menuDropdown = document.getElementById("menuDropdown") as HTMLDivElement;
-
-//   iframe.src = featurePath;
-//   featureTitle.textContent = featureName;
-//   menuDropdown.style.display = "none";
-// }
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   loadFeature(
-//     "target_allocation_maintenance/index.html",
-//     "Target Allocation Maintenance"
-//   );
-// });
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   console.log("Renderer script loaded");
-
-//   // Toggle menu
-//   const menuIcon = document.getElementById('menuIcon');
-//   menuIcon && menuIcon.addEventListener('click', toggleMenu);
-
-//   // Load features
-//   const loadFeature1 = document.getElementById('loadFeature1');
-//   loadFeature1 &&loadFeature1.addEventListener('click', () => loadFeature('target_allocation_maintenance/index.html', 'Target Allocation Maintenance'));
-
-//   const loadFeature2 = document.getElementById('loadFeature2');
-//   loadFeature2 && loadFeature2.addEventListener('click', () => loadFeature('feature_2/index.html', 'Another feature'));
-
-//   // Add more event listeners for other features as needed
-// });
-
-
-
+  return (
+    <div className="App">
+      <div className="header p-1">
+        <div className="menu-icon" onClick={() => toggleMenu()}>
+          <div className="menu-line"></div>
+          <div className="menu-line"></div>
+          <div className="menu-line"></div>
+        </div>
+        <span id="feature-title"></span>
+      </div>
+      <div className="menu-dropdown" id="menuDropdown">
+        <a
+          href="#"
+        >Target Allocation Maintenance</a>
+        <a
+          href="#"
+        >Another feature</a>
+      </div>
+      {dataRetreived &&
+        <TargetAllocationMaintenance Data={data} />
+      }
+    </div>
+  );
+};
 
 export default App;
