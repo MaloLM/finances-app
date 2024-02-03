eb5ef49479bff9b436440e5b91db6d731526bb77
wawkadura <elwalid.kadura@gmail.com>
2024-02-03 17:25:51 +0100

    feat(renderer/components) : add button fill animation

--- src/renderer/components/Button.tsx
+++ src/renderer/components/Button.tsx
@@ -4,15 +4,26 @@ interface ButtonProps {
     onClick?: () => void;
     children?: React.ReactNode;
     className?: string;
-    fill?: boolean;
+    filled?: boolean;
     disabled?: boolean;
     type?: "button" | "submit" | "reset";
 }
 
 export const Button = (props: ButtonProps) => {
     return (
-        <button disabled={props.disabled} type={props.type? props.type : "button"} className={"hover:brightness-110 transition-transform duration-100 hover:scale-105 rounded-full px-2 " +( props.fill ? "border-nobleGold text-lightNobleBlack bg-nobleGold hover:bg-nobleGold ": "border-nobleGold text-nobleGold hover:bg-nobleGold hover:text-lightNobleBlack ") + props.className} onClick={props.onClick}>
-            {props.children}
+        <button disabled={props.disabled}
+            type={props.type ? props.type : "button"}
+            className={` rounded-full px-2 
+        ${props.filled
+                    ? "border-nobleGold text-lightNobleBlack bg-nobleGold hover:bg-nobleGold hover:brightness-110 transition-transform duration-700 hover:scale-105 "
+                    : ` border-nobleGold text-nobleGold  
+                    relative flex transition-all items-center justify-center overflow-hidden before:absolute before:h-0 before:w-0 before:rounded-full before:bg-nobleGold before:duration-700 before:ease-in 
+                    hover:before:h-96 hover:before:w-96 hover:text-lightNobleBlack hover:bg-lightNobleBlack`} 
+                    ${props.className}`}
+            onClick={props.onClick}>
+            <div className="flex items-center justify-center z-30">
+                {props.children}
+            </div>
         </button>
     );
 }
